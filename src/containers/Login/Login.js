787d96c6ea25ec2a704c58cb9fcc8d985a7525b6
Avni Onur Pehlivan <avnionur@gmail.com>
2021-03-14 11:41:13 +0300

    upd login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -5,31 +5,30 @@ import css from './Login.module.css'
 
 function Login () {
   return (
-    <main main className={css.FormSignin}>
+    <main className={css.FormSignin}>
       <form className={css.Form}>
         <Logo height="157px" />
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-        <label for="inputEmail" class="visually-hidden">Email address</label>
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        <label className="visually-hidden">Email address</label>
         <input
           type="email"
           id="inputEmail"
-          class="form-control"
+          className="form-control"
           placeholder="Email address"
           required
-          autofocus
         />
-        <label for="inputPassword" class="visually-hidden">Password</label>
+        <label className="visually-hidden">Password</label>
         <input
           type="password"
           id="inputPassword"
-          class="form-control"
+          className="form-control"
           placeholder="Password"
           required
         />
-        <div class="checkbox mb-3">
+        <div className="checkbox mb-3">
           <label> <input type="checkbox" value="remember-me" /> Remember me </label>
         </div>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
       </form>
     </main >
 
